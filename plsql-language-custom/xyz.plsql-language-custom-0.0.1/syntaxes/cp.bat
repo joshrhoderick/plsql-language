be19bb89700bf0e03817e0af659af7ee0c426014
joshrhoderick <6166491+joshrhoderick@users.noreply.github.com>
2019-06-03 11:03:45 -0400

    Suppress message for xcopy

--- plsql-language-custom/xyz.plsql-language-custom-0.0.1/syntaxes/cp.bat
+++ plsql-language-custom/xyz.plsql-language-custom-0.0.1/syntaxes/cp.bat
@@ -1,1 +1,1 @@
-xcopy "..\..\xyz.plsql-language-custom-0.0.1" "%userprofile%\.vscode\extensions\xyz.plsql-language-custom-0.0.1" /e /y
+echo d | xcopy "..\..\xyz.plsql-language-custom-0.0.1" "%userprofile%\.vscode\extensions\xyz.plsql-language-custom-0.0.1" /e /y
